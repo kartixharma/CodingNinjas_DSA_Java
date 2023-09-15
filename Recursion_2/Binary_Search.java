c46e428fbff611e338b38bcedb3dc7839f3c7890
kartixharma <kartik.sh565656@gmail.com>
2023-09-16 00:29:29 +0530

    Binary Search

--- Recursion_2/Binary_Search.java
+++ Recursion_2/Binary_Search.java
@@ -1,18 +1,18 @@
-public class Solution {
-    public static int search(int []nums, int target) {
-        return bin(nums, target, 0, nums.length);
+public class solution {
+    public static int binarySearch(int []nums, int target) {
+        return bin(nums, target, 0, nums.length-1);
     }
     public static int bin(int[] nums, int target, int si, int ei){
-        if(si==ei){
+        if(si>ei){
             return -1;
         }
-        int mid=si+(ei-si)/2;
+        int mid=(ei+si)/2;
         if(target<nums[mid]){
-            return bin(nums, target, si, mid);
+            return bin(nums, target, si, mid-1);
         }
         if(target==nums[mid]){
             return mid;
         }
-            return bin(nums, target, mid+1, nums.length);
+            return bin(nums, target, mid+1, ei);
     }
 }
