65962706d216344c1930639592a2173bb17b477a
kartixharma <kartik.sh565656@gmail.com>
2023-11-27 19:54:23 +0530

    Search Node in BST

---
+++ BST - I/Search Node in BST.java
@@ -0,0 +1,13 @@
+public class Solution {
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+		if(root==null){
+			return false;
+		}
+		if(root.data==k)
+			return true;
+		else if(root.data>k)
+			return searchInBST(root.left, k);
+		else
+			return searchInBST(root.right, k);
+	}
+}
