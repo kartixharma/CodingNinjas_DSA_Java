e10b0848fa962de1473dc6614e6a8d96c335c744
kartixharma <kartik.sh565656@gmail.com>
2023-12-03 23:57:13 +0530

    Maximum Frequency Number

---
+++ Hashmaps/Maximum Frequency Number.java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.HashMap;
+public class Solution {
+    public static int maxFrequencyNumber(int[] arr){ 
+		HashMap<Integer, Integer> freq = new HashMap<>();
+		for(int n : arr){
+			if(freq.containsKey(n)){
+				int i = freq.get(n);
+				freq.put(n, i+1);
+				continue;
+			}
+			freq.put(n, 1);
+		}
+		int maxValue=0;
+		int maxKey=0;
+		for(int n : arr){
+			if(freq.get(n)>maxValue){
+				maxValue=freq.get(n);
+				maxKey=n;
+			}
+		}
+		return maxKey;
+	}
+}
