838111b893b51df16abbd137e9a858608f67a50d
kartixharma <kartik.sh565656@gmail.com>
2023-12-11 21:18:46 +0530

    Min Steps to One

---
+++ DP - 1/Min Steps to One.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public static int countMinStepsToOne(int n) {
+		if(n==1){
+			return 0;
+		}
+		int min=countMinStepsToOne(n-1);
+		if(n%2==0){
+			min= Math.min(min, countMinStepsToOne(n/2));
+		}
+		if(n%3==0){
+			min= Math.min(min, countMinStepsToOne(n/3));
+		}
+		return min+1;
+	}
+}
