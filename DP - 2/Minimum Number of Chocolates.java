3a42e72f37d5aa3d76eabf441f9a640f7899b0ff
kartixharma <kartik.sh565656@gmail.com>
2023-12-16 22:40:09 +0530

    Minimum Number of Chocolates

---
+++ DP - 2/Minimum Number of Chocolates.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public static int getMin(int arr[], int N){
+        int dp[]=new int[N+1];
+		int x=0;
+    	dp[0]=1;
+    	for(int i=1;i<N;i++){
+        if(arr[i]>arr[i-1])
+            dp[i]+=dp[i-1]+1;
+        else
+            dp[i]=1;
+    	}
+    	for(int i=N-2;i>=0;i--){
+        	if(arr[i]>arr[i+1] && dp[i]<=dp[i+1])
+            	dp[i]=dp[i+1]+1;
+		}
+    	for(int i=0;i<N;i++){
+        	x+=dp[i];
+   	 	}   
+    	return x;
+    }
+}
